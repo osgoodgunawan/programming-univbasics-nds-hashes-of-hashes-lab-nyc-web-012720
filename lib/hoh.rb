093e1ef3991b6de6827ed95932788cf4fcdc701e
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-16 21:52:06 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -33,6 +33,26 @@ PORTION_4 = {
 def naming_system
   
   naming_system={
+    label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class",
+        sub_category:{
+          label:"Order",
+          sub_category:{
+            label:"Family",
+            sub_category:{
+              label:"Genus",
+              sub_category:{
+                label:"Species",
+                sub_category:nil
+                }
+              }
+            }
+          }
+        }
+      }
   }
   
   naming_system
