430a92d8534296f33f917fafba092636fe339e33
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-16 21:23:26 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,13 +32,7 @@ PORTION_4 = {
 
 def naming_system
   
-  naming_system={
-    PORTION_1,
-    PORTION_2,
-    PORTION_3,
-    PORTION_4
-    
-  }
+  naming_system={}
   
   # Remember:
   #  Kingdom
